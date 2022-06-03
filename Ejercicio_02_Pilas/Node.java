dc8caac8f8af9acd5d6aa3e2c680d38ed8d6cbba
Mario-Chura <mchurapum@unsa.edu.pe>
2022-06-03 16:58:25 -0500

    Añadiendo metodos constructores

--- Ejercicio_02_Pilas/Node.java
+++ Ejercicio_02_Pilas/Node.java
@@ -3,4 +3,16 @@ package Ejercicio_02_Pilas;
 public class Node <T> {
 	private T dato;
 	private Node<T> siguiente;
+// Metodos constructores
+
+public Node(T data, Node<T> siguiente){
+    this.dato = data;
+    this.siguiente = siguiente;
+}
+public Node(T data){
+    this(data, null);
+}
+public Node(){
+    this(null, null);
+}
 }
