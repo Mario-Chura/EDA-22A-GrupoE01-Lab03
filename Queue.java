58c1905eedfccfabf5686d17e9b1bfb596b55feb
Icielo23 <viquise@unsa.edu.pe>
2022-06-04 00:03:16 -0500

    metodo add : añade un elemento a la cola o muestra un mensaje

--- Queue.java
+++ Queue.java
@@ -9,6 +9,7 @@ public class Queue<E> {
       this.cola = null;
       this.capacidad = n;
     }
+
     public boolean offer(E e){//Añade un elemento a Queue sin sobrepasar la capacidad de la cola
         if(tope == capacidad - 1)//Evalua si no se sobrepasa la capacidad de la cola
           return false;
@@ -21,5 +22,13 @@ public class Queue<E> {
         tope++;//incrementa el contador de los elementos presentes en la cola
         return true;
     }
+    public boolean add(E e){//Funciona igual que offer() con la diferencia que muestra un mensaje de limite maximo alcanzado
+        if(offer(e))//Si el elemento es añadido exitosamente devuelve true
+          return true;
+        else{//caso contratrio que la cola este llena mostrara un mensaje
+          System.out.println("Limite maximo alcanzado");
+          return false;//y se retornara false
+        }
+      }
 }
     
