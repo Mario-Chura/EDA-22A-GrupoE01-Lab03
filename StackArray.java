91ac4736ec580fdc091147ae4023e7887d53b621
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-06-04 10:34:05 -0500

    StackArray: variables de instancia y metodo constructor implementados

--- StackArray.java
+++ StackArray.java
@@ -1,4 +1,9 @@
-public class StackArray(){
-    
+public class StackArray <E> (){
+    private int tope;
+    private E[ ] array;
 
+    public StackArray(int n){
+        this.array = (E[ ]) new Object[n];
+        this.tope = -1;
+    }
 }
