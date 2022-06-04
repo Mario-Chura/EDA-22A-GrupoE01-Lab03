6a63586ca8bd155c8c587f881382a44c58ba5802
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-06-04 11:23:37 -0500

    StackArray: arreglo de la implementacion de Stack<E>

--- StackArray.java
+++ StackArray.java
@@ -1,9 +1,9 @@
-public class StackArray<E>{
+public class StackArray <E> implements Stack<E>{
     private int tope;
     private E[ ] array;
 
     public StackArray(int n){
-        this.array = (E[ ]) new Object[n];
+        this.array = (E[]) new Object[n];
         this.tope = -1;
     }
     public void push(E x){
@@ -17,6 +17,9 @@ public class StackArray<E>{
         return this.tope == -1;
         
     }
+    public E pop() throws ExceptionIsEmpty{
+        return null;
+    }
     public E top() throws ExceptionIsEmpty{
         if(isEmpty()){
            throw new ExceptionIsEmpty("Stack esta vacio..."); 
@@ -24,7 +27,12 @@ public class StackArray<E>{
         E item = this.array[this.tope];
         return item;
     }
-
-
+    public String toString(){
+        String str="";
+        for(int i=0;i<=this.tope;i++){
+            str += this.array[i]+", ";
+        }
+        return str;
+    }
 
 }
