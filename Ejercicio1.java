c96ae46bcbedeb7b5a93578c52f848debbd8723e
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-06-05 00:41:51 -0500

    implementado metodo equalStack en Ejercicio1

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -10,13 +10,36 @@ public class Ejercicio1{
             System.out.println("Tope: "+pila.top());
             pila.push(30);
             System.out.println("Tope: "+pila.top());
-
+            System.out.println(pila);
         }catch(ExceptionIsEmpty x){
             System.out.println(x.getMessage());
         }
             
         
     }
+    public static int equalStack(StackArray h1,StackArray h2,StackArray h3)throws ExceptionIsEmpty{
+        int altura=0;
+        int s1,s2,s3;        
+        boolean cond=true;
+        StackArray may;
+        while(cond){
+            //hacer pop a la pila con la mayor suma de los 3
+            //
+            System.out.println("suma p1:"+h1.summationStack());
+            System.out.println("suma p2:"+h2.summationStack());
+            System.out.println("suma p3:"+h3.summationStack());
+            System.out.println("--------------");
+            s1=h1.summationStack();
+            s2=h2.summationStack();
+            s3=h3.summationStack();
+            if(s1 == s2 && s1 == s3){
+                cond=false;
+            }
+        }
+        altura = h1.summationStack();//los 3 tendran la misma altura
+        return altura;
+    }
+
 
 
 }
