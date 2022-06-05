eb995fbd9dbba932ce716a8ce394aa3f7f9226a0
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-06-05 01:19:24 -0500

    movido a carpeta Ejercicio01

---
+++ Ejercicio01/StackArray.java
@@ -0,0 +1,49 @@
+public class StackArray <E> implements Stack<E>{
+    private int tope;
+    private E[ ] array;
+
+    public StackArray(int n){
+        this.array = (E[]) new Object[n];
+        this.tope = -1;        
+    }
+    public void push(E x){
+        if(this.tope < this.array.length - 1){
+            this.array[++this.tope] = x;
+        }else{
+            System.out.println("Stack esta lleno");
+        }
+    }
+    public boolean isEmpty (){
+        return this.tope == -1;
+        
+    }
+    public E pop() throws ExceptionIsEmpty{
+        if(isEmpty()){
+            throw new ExceptionIsEmpty("Stack esta vacio...");
+        }
+        E out = this.array[this.tope];
+        this.tope--;
+        return out;
+    }
+    public E top() throws ExceptionIsEmpty{
+        if(isEmpty()){
+           throw new ExceptionIsEmpty("Stack esta vacio..."); 
+        }
+        E item = this.array[this.tope];
+        return item;
+    }
+    public String toString(){
+        String str="";
+        for(int i=0;i<=this.tope;i++){
+            str += this.array[i]+", ";
+        }
+        return str;
+    }
+    public int summationStack(){
+        int suma =0;
+        for(int i=0;i<=this.tope;i++){
+            suma += (Integer)this.array[i];
+        }
+        return suma;
+    }
+}
